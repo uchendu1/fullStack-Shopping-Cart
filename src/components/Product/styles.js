b701b443dd6d5d2e41f335d9bd94671b03dc3690
linda <lindatochukwu30@gmail.com>
2022-09-14 13:19:13 +0100

    alignment of the products' component

--- src/components/Product/styles.js
+++ src/components/Product/styles.js
@@ -1,16 +1,22 @@
-import Styled from "styled-components";
+import styled from "styled-components";
 
-export const Container = Styled.div`
+export const Container = styled.div`
 flex: 1;
 margin: 5px;
 min-width: 280px;
 height: 350px
+display: flex;
+align-items: center;
+justify-content: center;
+background-color: #f5fbfd;
 `;
 
-export const Circle = Styled.img``;
+export const Circle = styled.div``;
 
-export const Image = Styled.img``;
+export const Image = styled.img`
+height: 75%;
+`;
 
-export const Info = Styled.div``;
+export const Info = styled.div``;
 
-export const Icon = Styled.div``;
+export const Icon = styled.div``;
--- src/components/Products/styles.js
+++ src/components/Products/styles.js
@@ -1,7 +1,9 @@
+import styled from "styled-components";
 
-import Styled from "styled-components"
+export const Container = styled.div`
+padding: 20px;
+display: flex;
+flex-wrap: wrap;
+justify-content: space-between;
 
-export const Container = Styled.div`
-
-
-`; 
+`;
